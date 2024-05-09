e2004b4df2093ba7b45a07776338e7dc27f77f21
Matthieu96Code <senoumatthieu96@gmail.com>
2024-05-09 11:28:51 +0000

    Style layout

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,9 +1,49 @@
 
 <nav class="sidebar">
-    <ul class="sidebar-menu menu-list">
-        <li><a wire:navigate href="{{ route('products') }}">Products</a></li>
-        <li><a wire:navigate href="{{ route('customers') }}">customers</a></li>
+    <a class="profile-link" href="">
+        <span class="main-icon user-icon">
+            <x-iconsax-out-profile-circle />
+        </span>
+        <h1>User</h1>
+    </a>
+    <h3 class="main-text menu-title">Menu</h3>
+    <ul class="menu-list">
+        <li class="menu-list-item">
+            <a wire:navigate class="menu-list-link" href="{{ url('users.home') }}">
+                <span class="main-icon menu-icon">
+                    <x-iconsax-lin-house />
+                </span>
+                <p>Accueil</p>
+            </a>
+        </li>
+        <li class="menu-list-item">
+            <a wire:navigate class="menu-list-link" href="{{ route('products') }}">
+                <span class="main-icon menu-icon">
+                    <x-iconsax-lin-card-add />
+                </span>
+                <p>Products</p>
+            </a>
+        </li>
+        <li class="menu-list-item">
+            <a wire:navigate class="menu-list-link" href="{{ route('customers') }}">
+                <span class="main-icon menu-icon">
+                    <x-iconsax-lin-document-text-1 />
+                </span>
+                <p>customers</p>
+            </a>
+        </li>
     </ul>
+
+    <form class="logout-form" action="{{ url('logout') }}" method="post">
+        @csrf
+        <button class="logout-btn" type="submit">
+            <span class="main-icon menu-icon">
+                <x-iconsax-two-logout />
+            </span>
+            <p>Déconnexion</p>
+        </button>
+    </form>
+
 </nav>
 
 {{-- <x-sidebar-modal name="sidebar" title="menu">
---
+++ resources/views/styles/old/layout.blade.php
@@ -0,0 +1,148 @@
+<style>
+    /* sidebar */
+
+    .user-section {
+        display: flex;
+    }
+
+    .sidebar {
+        display: flex;
+        flex-direction: column;
+        width: 21%;
+        background-color: var(--primary-background-color);
+        color : white;
+        padding: 0 30px;
+        min-height: 100vh;
+        border-radius: 0 30px 30px 0;
+        position: fixed;
+    }
+
+    .sidebar .profile-link {
+        padding-top: 30px;
+        display: flex;
+        gap: 24px;
+        margin-bottom: 50px;
+        align-items: center;
+    }
+
+    .sidebar .profile-link .user-icon {
+        width: 42px;
+    }
+
+    .sidebar .profile-link h1 {
+        color: white;
+    }
+
+    .sidebar .menu-title {
+        width: 8px;
+    }
+
+    .sidebar .menu-list {
+        display: flex;
+        flex-direction: column;
+        gap: 25px;
+        padding: 35px 0;
+        flex-grow: 1;
+    }
+
+    .sidebar .menu-list-link {
+        display: flex;
+        gap: 16px;
+        color: white;
+    }
+
+    .sidebar .logout-form {
+        margin-bottom: 20px;
+        flex-shrink: 1;
+    }
+
+    .sidebar .logout-form .logout-btn {
+        display: flex;
+        border: none;
+        background-color: transparent;
+        color: white;
+        align-items: center;
+        gap: 16px;
+        cursor: pointer;
+    }
+
+    /* main content */
+
+    .main-content {
+        width: 79%;
+        margin-left: 21%;
+        background-color: var(--secondary-background-color);
+        padding: 0 5%;
+        min-height: 100vh;
+    }
+
+    .main-content .navbar {
+        margin: 30px 0;
+        display: flex;
+        justify-content: space-between;
+        background-color: var(--primary-background-color);
+        border-radius: 9px;
+        padding: 9px 8%;
+    }
+
+    @media (max-width: 768px) {
+
+        .user-section {
+            flex-direction: column;
+        }
+
+        .sidebar {
+            display: grid;
+            grid-template-columns: 1fr auto;
+            width: 100%;
+            color : white;
+            padding: 0 30px;
+            border-radius: 0;
+            min-height: auto;
+        }
+
+        .sidebar .profile-link {
+            height: 50px;
+        }
+
+        .sidebar .menu-title {
+            display: none;
+        }
+        
+        .sidebar .menu-list {
+            border-top: 1px solid var(--secondary-background-color);
+            flex-direction: row;
+            overflow-x: scroll;
+            scroll-behavior:smooth;
+            grid-area: 2 / 1 / 3 / 3;
+        }
+        
+        .sidebar .logout-form {
+            grid-area: 1 / 2 / 1 / 2;
+            display: flex;
+        }   
+
+        .sidebar .menu-list li  {
+            text-wrap: nowrap;
+            background-color: white;
+            padding: 6px 12px;
+            border-radius: 12px;
+        }
+
+        .sidebar .menu-list a  {
+            text-wrap: nowrap;
+            color: var(--primary-text-color);
+        }
+
+        .main-content {
+            width: 100%;
+            margin-left: 0;
+            margin-top: 210px;
+        }
+
+    }
+
+    @media (max-width: 1024px) {
+
+    }
+</style>
