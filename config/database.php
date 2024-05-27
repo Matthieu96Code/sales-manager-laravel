1e43bd1eda049a8fe7c34f525708a2f508a41d13
Kodjo Matthieu SENOU <senoumatthieu96@gmail.com>
2024-05-27 02:40:47 +0000

    Revert "Install config mongodb"
    This reverts commit 512026356c04d62b73ec311e643fdc9e0873302f.

--- config/database.php
+++ config/database.php
@@ -68,14 +68,14 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'mydb'),
-            'username' => env('DB_USERNAME', 'postgres'),
-            'password' => env('DB_PASSWORD', 'password'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
@@ -91,13 +91,6 @@ return [
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
-        ],
-        
-        'mongodb' => [
-            'driver' => 'mongodb',
-                'dsn' => env('DB_URI', 'mongodb+srv://username:password@<atlas-cluster-uri>/myappdb?retryWrites=true&w=majority'),
-                // 'database' => 'myappdb',
-                'database' => env('DB_DATABASE', 'forge'),
         ],
 
     ],
