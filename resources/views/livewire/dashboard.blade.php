0d541a276d494ede727194e9be214d431bc72fb7
Kodjo Matthieu SENOU <senoumatthieu96@gmail.com>
2024-06-21 08:40:57 +0000

    Update guest dashboard on red text

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -15,17 +15,19 @@
     @endif
 
     @if (Auth::user()->role === 0)
-       <div class="guest-log">
-         <p>
-             Your account is unactivated
-         </p>
-         <p>
-             In order to activate your account, please go to the profile section and click on the activate button.
-             You will enter the provided registration code and your account will be activated.
-         </p>
-         <p>
-             In case you have not received the registration code, please contact the administrator.
-         </p>
-       </div>
+        <div class="guest-log">
+            <p class="red-text">Your account is unactivated</p>
+            <br>
+
+            <p class="red-text">
+                In order to activate your account, please go to the profile section and click on the activate button.
+                You will enter the provided registration code and your account will be activated.
+            </p>
+            <br>
+            
+            <p class="red-text">
+                In case you have not received the registration code, please contact the administrator.
+            </p>
+        </div>
     @endif
 </div>
