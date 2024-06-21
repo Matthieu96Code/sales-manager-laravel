a723256aca608368b4dffa7bbfa0944718a5cc88
Kodjo Matthieu SENOU <senoumatthieu96@gmail.com>
2024-06-21 08:37:43 +0000

    Create a dummy secrete code for account activation

--- app/Livewire/Profile.php
+++ app/Livewire/Profile.php
@@ -13,70 +13,81 @@ class Profile extends Component
 
     public function desactive() {
 
-        if (true) {
+        if ($this->secretKey === '0000') {
             User::find(Auth::user()->id)->update(
                 [
                     'role' => 0,
                 ]
             );
 
-            $this->reset('secretKey');
             session()->flash('success', 'your are desactivated successfully');
             $this->dispatch('close-modal');
             return $this->redirect('/profile',navigate:true);
-
+        } else {
+            session()->flash('error', 'your secret key is wrong');
         }
+
+        $this->reset('secretKey');
     }
 
     public function active() {
 
-        if ((Auth::user()->role === 0) && true) {
+        if ((Auth::user()->role === 0) && ($this->secretKey === '0000')) {
             User::find(Auth::user()->id)->update(
                 [
                     'role' => 1,
                 ]
             );
 
-            $this->reset('secretKey');
             session()->flash('success', 'your are activated successfully');
             $this->dispatch('close-modal');
             return $this->redirect('/profile',navigate:true);
 
+        } else {
+            session()->flash('error', 'your secret key is wrong');
         }
+
+        $this->reset('secretKey');
     }
 
     public function upgrade() {
 
-        if ((Auth::user()->role === 1) && true) {
+        if ((Auth::user()->role === 1) && ($this->secretKey === '0000')) {
             User::find(Auth::user()->id)->update(
                 [
                     'role' => 2,
                 ]
             );
 
-            $this->reset('secretKey');
             session()->flash('success', 'your are upgraded successfully');
             $this->dispatch('close-modal');
             return $this->redirect('/profile',navigate:true);
 
+        } else {
+            session()->flash('error', 'your secret key is wrong');
         }
+
+        $this->reset('secretKey');
     }
 
     public function downgrade() {
 
-        if ((Auth::user()->role === 2) && true) {
+        if ((Auth::user()->role === 2) && ($this->secretKey === '0000')) {
             User::find(Auth::user()->id)->update(
                 [
                     'role' => 1,
                 ]
             );
 
-            $this->reset('secretKey');
             session()->flash('success', 'your are downgraded successfully');
             $this->dispatch('close-modal');
             return $this->redirect('/profile',navigate:true);
 
+        } else {
+            session()->flash('error', 'your secret key is wrong');
         }
+
+        $this->reset('secretKey');
     }
 
     public function render()
