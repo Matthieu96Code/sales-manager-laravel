a3822a23f1f7d0bc767c6fc0928bee491a3ff76c
Kodjo Matthieu SENOU <senoumatthieu96@gmail.com>
2024-06-22 09:34:12 +0000

    Create an history model

---
+++ app/Models/History.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'sale_id',
+        'supply_id',
+    ];
+
+    public function scopeSearch($query, $value){
+        $query->where('product_id', 'like', "%{$value}%")
+        ->orwhere('user_id', 'like', "%{$value}%")
+        ->orwhere('quantity', 'like', "%{$value}%");
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    public function sale(){
+        return $this->belongsTo(Sale::class);
+    }
+
+    public function supply(){
+        return $this->belongsTo(Supply::class);
+    }
+}
